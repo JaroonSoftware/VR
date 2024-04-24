68c04414da0b391666d3710f852640242f102f8c
SORRANUN BUMRUNGPONG <gaiougame000000@gmail.com>
2024-04-24 16:55:51 +0700

    24/04/2024

--- src/nav.js
+++ src/nav.js
@@ -1,24 +1,17 @@
 import {
-  // AppstoreOutlined,
   FileTextFilled,
   FileDoneOutlined, 
   FileProtectOutlined,
-  HddOutlined,
-  // InboxOutlined,
   TeamOutlined,
-  // UserOutlined, 
   ExperimentOutlined,
   ReconciliationFilled,
-  //FileTextOutlined,
 } from "@ant-design/icons";
  
 import { TbReportMoney } from "react-icons/tb";
-import { FaBottleDroplet, FaBoxesPacking } from "react-icons/fa6"; 
+import { FaBoxesPacking } from "react-icons/fa6"; 
 import { RiTeamFill } from "react-icons/ri";
 import { MdOutlineDashboard } from "react-icons/md";
-import { FaTruckLoading } from "react-icons/fa";
 import { 
-  MdOutlineGroup, 
   MdRequestQuote 
 } from "react-icons/md";
 let _nav = [
@@ -27,7 +20,7 @@ let _nav = [
     type: "group",
   },
   {
-    title: "Dashboard",
+    title: "หน้าหลัก",
     icon: <MdOutlineDashboard className="nav-ico" />,
     to: "/dashboard",
   },
@@ -36,108 +29,40 @@ let _nav = [
     type: "group",
   },
   {
-    title: "Sample Request",
+    title: "ใบเสนอราคา",
     icon: <FileTextFilled className="nav-ico" />, 
     to: "/sample-request",
   },
   {
-    title: "Sample Preparation",
+    title: "ใบขายสินค้า",
     icon: <ReconciliationFilled className="nav-ico" />, 
     to: "/sample-preparation",
   },
   {
-    title: "Pilot Scale",
-    icon: <ExperimentOutlined className="nav-ico" />, 
-    to: "/pilot-scale",
-  },
-  {
-    title: "Delivery Note",
-    icon: <FileProtectOutlined  className="nav-ico"/>, 
-    to: "/delivery-note",
-  },
-  {
-    title: "Estmate Cost",
+    title: "ใบเสร็จรับเงิน",
     icon: <TbReportMoney  className="nav-ico"/>, 
     to: "/estimation",
   },
-  {
-    title: "Packing Set",
-    icon: <FaBoxesPacking  className="nav-ico"/>,
-    to: "/packing-set",
-  },
-  {
-    title: "Quotation",
-    icon: <MdRequestQuote  className="nav-ico"/>,
-    to: "/quotation",
-  },
-
-
-
   {
     title: "MASTER",
     type: "group",
   },  
   {
-    title: "Items",
+    title: "ข้อมูลสินค้า",
     icon: <FileDoneOutlined className="nav-ico" />,
     to: "/items",
   },
   {
-    title: "Packaging",
-    icon: <FaBottleDroplet className="nav-ico" />,
-    to: "/packaging",
-  },
-  {
-    title: "Shipping Type",
-    icon: <FaTruckLoading className="nav-ico" />,
-    to: "/shipping-type",
-  },
-
-  {
-    title: "Unit",
-    icon: <HddOutlined className="nav-ico" />,
-    to: "/unit",
-  },
-  // {
-  //   title: "Product Type",
-  //   icon: <InboxOutlined className="nav-ico" />,
-  //   to: "/itemtype",
-  // },
-  {
-    title: "Customers",
+    title: "ข้อมูลลูกค้า",
     icon: <RiTeamFill className="nav-ico" />,
     to: "/customers",
   },
-  // {
-  //   title: "Customer",
-  //   icon: <UserOutlined className="nav-ico" />,
-  //   to: "/customer",
-  // },
   {
-    title: "Suppliers",
-    icon: <MdOutlineGroup className="nav-ico" />,
-    to: "/suppliers",
-  },  
-  // {
-  //   title: "Supplier",
-  //   icon: <UserOutlined className="nav-ico" />,
-  //   to: "/supplier",
-  // },  
-  {
-    title: "User",
+    title: "ผู้ใช้งาน",
     icon: <TeamOutlined className="nav-ico" />,
     to: "/user",
   },
-  // {
-  //   title: "File",
-  //   icon: <TeamOutlined className="nav-ico" />,
-  //   to: "/file-control",
-  // },
-  // {
-  //   title: "Sample Request [ Dev ]",
-  //   icon: <FileTextOutlined />,
-  //   to: "/sample-request",
-  // },
+
 ];
 
 export default _nav;
